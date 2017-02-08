07912be206098a8e36aeeee6e9a804b655644fc4
rghossi <rafael.ghossi@gmail.com>
2017-02-08 16:48:37 -0200

    added results route

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route component={App}>
   <Route path="/" component={Voting} />
+  <Route path="/results" component={Results} />
 </Route>;
 
 ReactDOM.render(
